ff3df7387dc60332c6e3186c35490b04b1f79bee
Pierre Lourens <plourens3@gmail.com>
2020-08-05 12:40:30 -0400

    adds redux set up

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+import ReduxPromise from "redux-promise";
+import reducers from "./reducers";
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
-
