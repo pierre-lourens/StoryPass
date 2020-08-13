32a11483bbfe46ed0c10e93f10d3c0c3d8590066
Pierre Lourens <plourens3@gmail.com>
2020-08-13 08:20:24 -0400

    Updates backend for prompts - Adds a route to select 1 randomly generated prompt from prompts table - Adds a route to post new prompts - Note, you'll need to add a prompts table to db. - Set it up so that we can eventually have more props than prompt text

--- server/router.js
+++ server/router.js
@@ -1,11 +1,18 @@
 module.exports = function (app) {
   // USER ROUTES
   const users = require("./controllers/UserController");
+  const prompts = require("./controllers/PromptsController");
 
   // retrieve all users
   app.get("/api/users", users.findAll);
 
   app.post("/api/users", users.addText);
+
+  // return a prompt from the prompts table
+  app.get("/api/prompt", prompts.getRandomPrompt);
+
+  // save a new prompt
+  app.post("/api/prompt", prompts.addPrompt);
 
   app.use((req, res) => {
     res.status(404).send("404 error! Resource not found.");
--- server/user_data.sql
+++ server/user_data.sql
@@ -4,5 +4,8 @@ drop table if exists Users;
 /* Create the schema for our tables */
 create table Users(userName varchar(30), text varchar(100));
 
+/* Create the schema for our prompts */
+create table prompts(prompt varchar(180));
+
 /* Populate the tables with our data */
 insert into Users values('TestUser', 'Test text');
